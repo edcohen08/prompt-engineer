881bb4deb2eda017c67fce9f514f534e714794bf
Eddie Cohen <eddie@lightmatter.com>
2023-04-29 00:14:43 -0400

    zero shot pipeline

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="AI Prompt Engineer", page_icon="🦫")
+st.set_page_config(page_title="AI Prompt Engineer", page_icon="🦫", layout="wide")
+
+from models import call_zero_shot_pipeline, convert_df
 
 if 'prompt_count' not in st.session_state:
     st.session_state.prompt_count = 0
@@ -8,16 +10,50 @@ if 'prompt_count' not in st.session_state:
 if 'prompt_0' not in st.session_state:
     st.session_state['prompt_0'] = ""
 
+if 'demonstration_count' not in st.session_state:
+    st.session_state.demonstration_count = 0
+
+if 'question_0' not in st.session_state:
+    st.session_state['question_0'] = ""
+
+if 'answer_0' not in st.session_state:
+    st.session_state['answer_0'] = ""
+
 st.header("AI Prompt Engineer")
-col_1, col_2 = st.columns(2)
+col_1, col_2, col_3 = st.columns(3, gap="medium")
 
-for i in range(0, st.session_state.prompt_count + 1):
-    with col_1:
-        st.text_input(f"Prompt Candidate {i + 1}", help="no help", key=f"prompt_{i}", placeholder=f"Prompt candidate {i+1}", label_visibility="collapsed")
-    with col_2:
-        st.button('Remove prompt', key=f"remove_prompt_{i}")
+with st.form("prompt_test"):
+    for i in range(0, st.session_state.prompt_count + 1):
+        with col_1:
+            st.text_area(f"Prompt Candidate {i + 1}", help="no help", key=f"prompt_{i}", placeholder=f"Prompt candidate {i+1}", label_visibility="collapsed")
 
-if st.button('Add prompt', type="primary"):
-    st.session_state.prompt_count += 1
-    st.session_state[f"prompt_{st.session_state.prompt_count}"] = ""
-    st.experimental_rerun()
+    for i in range(0, st.session_state.demonstration_count + 1):
+        with col_2:
+            st.text_area(f"Question {i + 1}", help="no help", key=f"question_{i}", placeholder=f"question {i+1}", label_visibility="collapsed")
+        with col_3:
+            st.text_area(f"Answer {i + 1}", help="no help", key=f"answer_{i}", placeholder=f"answer {i+1}", label_visibility="collapsed")
+
+    if st.form_submit_button():
+        global df
+        df = call_zero_shot_pipeline(dict(st.session_state))
+        st.dataframe(df)
+
+
+st.download_button(
+    label="Download data as CSV",
+    data=convert_df(df),
+    file_name='results.csv',
+    mime='text/csv',
+)
+
+with col_1:
+    if st.button('Add', type="primary", key="add_prompt"):
+        st.session_state.prompt_count += 1
+        st.session_state[f"prompt_{st.session_state.prompt_count}"] = ""
+        st.experimental_rerun()
+
+with col_2:
+    if st.button('Add', type="primary", key="add_demonstration"):
+        st.session_state.demonstration_count += 1
+        st.session_state[f"question_{st.session_state.demonstration_count}"] = ""
+        st.experimental_rerun()
---
+++ models.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+from langchain.chains import LLMChain, SequentialChain
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.prompts.few_shot import FewShotPromptTemplate
+from pandas import DataFrame
+
+@st.cache_resource
+def load_evaluator_chain():
+    template = """Give a score 0-100 to the AI output for how exactly it matches the ground truth.
+    Do not penalize exact matches. Ground truth: {answer} AI: {ai_answer}"""
+    prompt = PromptTemplate(
+        input_variables=["answer", "ai_answer"],
+        template=template,
+    )
+    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    return LLMChain(llm=llm, prompt=prompt, output_key="score")
+
+@st.cache_resource
+def load_zero_shot_chain():
+    template = """{prompt_candidate}. Q: {input} A:"""
+
+    prompt = PromptTemplate(
+        input_variables=["prompt_candidate", "input"],
+        template=template
+    )
+    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0)
+    return LLMChain(llm=llm, prompt=prompt, output_key="ai_answer")
+
+@st.cache_resource
+def load_zero_shot_pipeline():
+    zero_shot = load_zero_shot_chain()
+    evaluator = load_evaluator_chain()
+    return SequentialChain(
+        chains=[zero_shot, evaluator],
+        input_variables=["prompt_candidate", "input", "answer"],
+        output_variables=["ai_answer", "score"]
+    )
+
+@st.cache_data
+def call_zero_shot_pipeline(state: dict) -> DataFrame:
+    zero_shot_pipeline = load_zero_shot_pipeline()
+    results = []
+    for i in range(st.session_state["prompt_count"] + 1):
+        for j in range(st.session_state["demonstration_count"] + 1):
+            results.append(zero_shot_pipeline({"prompt_candidate": st.session_state[f"prompt_{i}"], "input": st.session_state[f"question_{j}"], "answer": st.session_state[f"answer_{j}"]}))
+    
+    return DataFrame(results) 
+
+@st.cache_data
+def convert_df(df: DataFrame) -> str:
+    return df.to_csv().encode("utf-8")
+
+@st.cache_resource
+def load_few_shot_chain():
+    pass
+
+
